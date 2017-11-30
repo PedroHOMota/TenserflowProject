ba7dce72782454ed3216be8a0385e0c91d970953
PedroHOMota <phmota@hotmail.com>
2017-11-30 19:21:56 +0000

    Added coments

--- DigitRecognizerKeras.py
+++ DigitRecognizerKeras.py
@@ -3,11 +3,10 @@ import numpy as np
 from keras.models import load_model
 
 model = load_model('keras-mnist.h5')
-print("chegou aqui")
-def recnoizeDigit(data):
-    data = np.ndarray.flatten(data)
-    data=data.reshape(1,784)
-    print(data)
+
+def recnoizeDigit(data): #The image has to be an array 28x28
+    data = np.ndarray.flatten(data) #Transforms the 2d array into a 1d array row major
+    data=data.reshape(1,784) 
     prediction = np.around(model.predict_classes(data)).astype(np.int)[0]
     print(prediction)
     return prediction
